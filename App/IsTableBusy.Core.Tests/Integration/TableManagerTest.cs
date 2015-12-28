35564bcc415f3232af7d67a7f230e4a4ad0ba74b
Jerzy Wickowski <tazos333@gmail.com>
2015-12-28 11:00:00 +0100

    add simple TableManager

---
+++ App/IsTableBusy.Core.Tests/Integration/TableManagerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IsTableBusy.EntityFramework;
+using IsTableBusy.EntityFramework.Model;
+using Xunit;
+
+namespace IsTableBusy.Core.Tests.Integration
+{
+    public class TableManagerTest : IsTableBusyDatabaseTest, IDisposable
+    {
+        [Fact]
+        public void SetBusy()
+        {
+            using (Context ctx = new Context())
+            {
+                var testTable = ctx.Tables.First();
+                testTable.IsBusy = false;
+                ctx.SaveChanges();
+
+                TableManager tm  = new TableManager(ctx);
+                tm.SetBusy(testTable.Id);
+
+                var result = ctx.Tables.Single(x => x.Id == testTable.Id);
+                result.IsBusy.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void SetFree()
+        {
+            using (Context ctx = new Context())
+            {
+                var testTable = ctx.Tables.First();
+                testTable.IsBusy = true;
+                ctx.SaveChanges();
+
+                TableManager tm = new TableManager(ctx);
+                tm.SetFree(testTable.Id);
+
+                var result = ctx.Tables.Single(x => x.Id == testTable.Id);
+                result.IsBusy.Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ App/IsTableBusy.Core/TableManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IsTableBusy.EntityFramework;
+
+namespace IsTableBusy.Core
+{
+    public class TableManager
+    {
+        private Context context;
+
+        public TableManager(Context context)
+        {
+            this.context = context;
+        }
+
+        public void SetBusy(int tableId)
+        {
+            ChangeIsBusy(tableId, true);
+        }
+
+        public void SetFree(int tableId)
+        {
+            ChangeIsBusy(tableId, false);
+        }
+
+        private void ChangeIsBusy(int tableId, bool isBusy)
+        {
+            var table = context.Tables
+              .Single(x => x.Id == tableId);
+
+            table.IsBusy = isBusy;
+
+            context.SaveChanges();
+        }
+    }
+}
