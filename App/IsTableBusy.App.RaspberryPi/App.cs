2776af4339c8dcbb1be1e9af740b102ff32c9495
Jerzy Wickowski <tazos333@gmail.com>
2015-12-29 20:00:41 +0100

    move logic to app class

---
+++ App/IsTableBusy.App.RaspberryPi/App.cs
@@ -0,0 +1,39 @@
+﻿using IsTableBusy.App.RaspberryPi.Common;
+
+namespace IsTableBusy.App.RaspberryPi
+{
+    public sealed class App
+    {
+        private Led redLed;
+        private Button button;
+        private Led greenLed;
+
+        public void Run()
+        {
+            greenLed = new Led(5);
+            redLed = new Led(6);
+            button = new Button(22);
+
+            button.Clicked += (s, e) =>
+            {
+                ChangeLed();
+            };
+
+            ChangeLed();
+        }
+
+        private void ChangeLed()
+        {
+            if (greenLed.IsOn)
+            {
+                greenLed.Off();
+                redLed.Off();
+            }
+            else
+            {
+                greenLed.On();
+                redLed.On();
+            }
+        }
+    }
+}
--- App/IsTableBusy.App.RaspberryPi/StartupTask.cs
+++ App/IsTableBusy.App.RaspberryPi/StartupTask.cs
@@ -1,47 +1,15 @@
 ﻿using Windows.ApplicationModel.Background;
-using IsTableBusy.App.RaspberryPi.Common;
-
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace IsTableBusy.App.RaspberryPi
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        private Led greenLed;
-        private Led redLed;
-        private Button button22;
         private BackgroundTaskDeferral deferral;
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
-
-            greenLed = new Led(5);
-            redLed = new Led(6);
-            button22 = new Button(22);
-
-
-            button22.Clicked += (s, e) =>
-            {
-                ChangeLed();
-            };
-
-            ChangeLed();
-        }
-        
-        private void ChangeLed()
-        {
-
-            if (greenLed.IsOn)
-            {
-                greenLed.Off();
-                redLed.Off();
-            }
-            else
-            {
-                greenLed.On();
-                redLed.On();
-            }
-           
+            var app = new App();
+            app.Run();
         }
     }
 }
