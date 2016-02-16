33cc20f12072de0a4a4790a9b3603c07d81f5272
Jerzy Wickowski <tazos333@gmail.com>
2016-02-16 13:49:10 +0100

    add simple devices controlles

---
+++ App/IsTableBusy.App.Api/Controllers/DevicesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IsTableBusy.Core;
+using IsTableBusy.Core.Models;
+
+namespace IsTableBusy.App.Api.Controllers
+{
+    public class DevicesController : ApiController
+    {
+        private readonly DeviceRegister deviceRegister;
+
+        public DevicesController(DeviceRegister deviceRegister)
+        {
+            this.deviceRegister = deviceRegister;
+        }
+
+        [HttpPost]
+        [Route("devices/register/{guid:Guid}")]
+        public DeviceRegisterViewModel Register(Guid guid)
+        {
+            var result  = this.deviceRegister.Register(guid);
+            return new DeviceRegisterViewModel {Guid = result};
+        }
+
+        [HttpPost]
+        [Route("devices/register")]
+        public DeviceRegisterViewModel Register()
+        {
+            var result = this.deviceRegister.Register();
+            return new DeviceRegisterViewModel { Guid = result };
+        }
+
+    }
+}
---
+++ App/IsTableBusy.Core/Models/DeviceRegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsTableBusy.Core.Models
+{
+    public class DeviceRegisterViewModel
+    {
+        public Guid Guid { get; set; }
+    }
+}
