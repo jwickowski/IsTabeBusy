983776ab32ef3d19ffc18076ceb084c118a413a2
Jerzy Wickowski <tazos333@gmail.com>
2016-03-16 20:29:41 +0100

    add deviceStareReader

---
+++ App/IsTableBusy.Core.Tests/Integration/Devices/DeviceStateReaderTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using IsTableBusy.Core.Devices;
+using IsTableBusy.Core.Tests.LoadData;
+using IsTableBusy.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IsTableBusy.Core.Tests.Integration.Devices
+{
+    public class DeviceStateReaderTest
+    {
+        [Fact]
+        public void get_free()
+        {
+            using (Context context = new Context())
+            {
+                var loader = new DeviceDrivenTestDataLoader(context);
+                var loadedData = loader.Load();
+                var tableWithFreeDevice = loadedData.TableWithFreeDevice;
+                var deviceStateReader = new DeviceStateReader(context);
+
+                bool result = deviceStateReader.Read(tableWithFreeDevice.Device.Guid);
+                result.Should().Be(false);
+            }
+        }
+
+        [Fact]
+        public void get_busy()
+        {
+            using (Context context = new Context())
+            {
+                var loader = new DeviceDrivenTestDataLoader(context);
+                var loadedData = loader.Load();
+                var tableWithBusyDevice = loadedData.TableWithBusyDevice;
+                var deviceStateReader = new DeviceStateReader(context);
+
+                bool result = deviceStateReader.Read(tableWithBusyDevice.Device.Guid);
+                result.Should().Be(true);
+            }
+        }
+    }
+}
---
+++ App/IsTableBusy.Core/Devices/DeviceStateReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IsTableBusy.EntityFramework;
+
+namespace IsTableBusy.Core.Devices
+{
+    public class DeviceStateReader
+    {
+        private Context context;
+
+        public DeviceStateReader(Context context)
+        {
+            this.context = context;
+        }
+
+        public bool Read(Guid guid)
+        {
+            var table = context.Tables.Single(x => x.Device.Guid == guid);
+            return table.IsBusy;
+        }
+    }
+}
