6348c956f829e39cd70c79d3df96cdc3fdabca35
Jerzy Wickowski <tazos333@gmail.com>
2016-03-16 19:38:19 +0100

    save guid to file

--- App/IsTableBusy.Device.Core/Logic/Config.cs
+++ App/IsTableBusy.Device.Core/Logic/Config.cs
@@ -1,19 +1,37 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Threading.Tasks;
-using Windows.Storage;
 
 namespace IsTableBusy.Device.Core.Logic
 {
-    public sealed  class Config
+    public sealed class Config
     {
+        SerializableStorage storage;
+        public Config()
+        {
+            storage = new SerializableStorage("IsTableBusy.config.xml");
+        }
         public string ApiUrl => "http://192.168.0.101/api";
 
-        public Guid DeviceGuid { get; set; }
+        public Guid DeviceGuid
+        {
+            get
+            {
+                return storage.Load().DeviceGuid;
+            }
+            set
+            {
+                var data = storage.Load();
+                data.DeviceGuid = value;
+                storage.Save(data);
+            }
+        }
 
         public string PlaceName { get; set; }
 
         public int TableId { get; set; }
+    }
+
+    public sealed class ConfigData
+    {
+        public Guid DeviceGuid { get; set; }
     }
 }
---
+++ App/IsTableBusy.Device.Core/Logic/SerializableStorage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace IsTableBusy.Device.Core.Logic
+{
+    public sealed class SerializableStorage
+    {
+        private string fileName;
+        public SerializableStorage(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Save(ConfigData data)
+        {
+            var saveTask = SaveAsync(this.fileName, data);
+            saveTask.Wait();
+        }
+
+        private static async Task SaveAsync(string FileName, ConfigData _Data) 
+        {
+            MemoryStream _MemoryStream = new MemoryStream();
+            
+            var Serializer = new System.Xml.Serialization.XmlSerializer(typeof(ConfigData));
+            Serializer.Serialize(_MemoryStream, _Data);
+
+            Task.WaitAll();
+
+            StorageFile _File = await ApplicationData.Current.LocalFolder.CreateFileAsync(FileName, CreationCollisionOption.ReplaceExisting);
+
+            using (Stream fileStream = await _File.OpenStreamForWriteAsync())
+            {
+                _MemoryStream.Seek(0, SeekOrigin.Begin);
+                await _MemoryStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+                fileStream.Dispose();
+            }
+        }
+
+        public ConfigData Load()
+        {
+            var saveTask = LoadAsync(this.fileName);
+            return saveTask.Result;
+        }
+
+        private static async Task<ConfigData> LoadAsync(string FileName)
+        {
+            StorageFolder _Folder = ApplicationData.Current.LocalFolder;
+            StorageFile _File;
+            ConfigData Result;
+
+            try
+            {
+                Task.WaitAll();
+                _File = await _Folder.GetFileAsync(FileName);
+
+                using (Stream stream = await _File.OpenStreamForReadAsync())
+                {
+                    var Serializer = new System.Xml.Serialization.XmlSerializer(typeof(ConfigData));
+
+                    Result = (ConfigData)Serializer.Deserialize(stream);
+                }
+                return Result;
+            }
+            catch (Exception ex)
+            {
+                return new ConfigData();
+            }
+        }
+    }
+}
