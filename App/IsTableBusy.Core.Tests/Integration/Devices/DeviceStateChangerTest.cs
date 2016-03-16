4e08268c0f0206966382958e006177a617336489
Jerzy Wickowski <jwickowski@objectivity.co.uk>
2016-03-16 15:00:57 +0100

    handle some exceptions in DeviceStateChanger

--- App/IsTableBusy.Core.Tests/Integration/Devices/DeviceStateChangerTest.cs
+++ App/IsTableBusy.Core.Tests/Integration/Devices/DeviceStateChangerTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using IsTableBusy.Core.Devices;
+using IsTableBusy.Core.Exceptions;
 using IsTableBusy.Core.Tests.LoadData;
 using IsTableBusy.EntityFramework;
 using Xunit;
@@ -15,7 +16,7 @@ namespace IsTableBusy.Core.Tests.Integration.Devices
     {
         public DeviceStateChangerTest()
         {
-         //   DateTimeSupplier.Date = new DateTime(2015, 3, 4);
+            //   DateTimeSupplier.Date = new DateTime(2015, 3, 4);
         }
 
         [Fact]
@@ -51,6 +52,36 @@ namespace IsTableBusy.Core.Tests.Integration.Devices
                 var table = context.Tables.Single(x => x.Id == tableWithBusyDevice.Id);
 
                 table.IsBusy.Should().Be(false);
+            }
+        }
+
+        [Fact]
+        public void device_is_not_exists()
+        {
+            using (Context context = new Context())
+            {
+
+                var deviceStateChanger = new DeviceStateChanger(context);
+
+                Action a = () => { deviceStateChanger.SetBusy(Guid.NewGuid(), false); };
+
+                a.ShouldThrow<ChangingDeviceStateException>();
+            }
+        }
+
+        [Fact]
+        public void device_is_not_connected()
+        {
+            using (Context context = new Context())
+            {
+                var loader = new DeviceDrivenTestDataLoader(context);
+                var loadedData = loader.Load();
+
+                var deviceStateChanger = new DeviceStateChanger(context);
+
+                Action a = () => { deviceStateChanger.SetBusy(loadedData.NotConnectedDevice.Guid, false); };
+
+                a.ShouldThrow<ChangingDeviceStateException>();
             }
         }
     }
--- App/IsTableBusy.Core/Devices/DeviceStateChanger.cs
+++ App/IsTableBusy.Core/Devices/DeviceStateChanger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IsTableBusy.Core.Exceptions;
 using IsTableBusy.EntityFramework;
 
 namespace IsTableBusy.Core.Devices
@@ -19,6 +20,10 @@ namespace IsTableBusy.Core.Devices
         public void SetBusy(Guid guid, bool isBusy)
         {
             var table = context.Tables.SingleOrDefault(x => x.Device.Guid == guid);
+            if (table == null)
+            {
+                throw new ChangingDeviceStateException();
+            }
             table.IsBusy = isBusy;
             context.SaveChanges();
         }
