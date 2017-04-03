a6bd911177c5554f52feca3336aa25d21617c1ec
Jerzy Wickowski <jerzy.wickowski@gmail.com>
2017-04-03 12:03:14 +0200

    turn on/off leb by pressing button withoud debounsing

--- App/IsTableBusy.ESP2866/src/Blink.cpp
+++ App/IsTableBusy.ESP2866/src/Blink.cpp
@@ -2,20 +2,30 @@
 
 #define LED 04
 #define BUTTON_TOP 0
-
+bool previousButtonState;
+bool previousLedState;
 void setup()
 {
   pinMode(LED, OUTPUT);
   pinMode(BUTTON_TOP, INPUT);
+  previousButtonState = digitalRead(BUTTON_TOP);
+  digitalWrite(LED, LOW);
+  previousLedState = LOW;
 }
 
 void loop()
 {
-  bool buttonStatus =  digitalRead(BUTTON_TOP);
-  if(buttonStatus == HIGH){
-    digitalWrite(LED, LOW);
+  bool buttonState =  digitalRead(BUTTON_TOP);
+  if(previousButtonState == LOW && buttonState == HIGH)
+  {
+    if(previousLedState == HIGH){
+      digitalWrite(LED, LOW);
+      previousLedState = LOW;
+    }
+    else{
+      digitalWrite(LED, HIGH);
+      previousLedState = HIGH;
+    }
   }
-  else{
-    digitalWrite(LED, HIGH);
-  }
+  previousButtonState = buttonState;
 }
