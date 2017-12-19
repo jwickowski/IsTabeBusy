cbdb442db86197aa58cac36c77a927bd0170e7f3
Jerzy Wickowski <tazos333@gmail.com>
2017-12-19 09:45:31 +0100

    add DbMigrationHelpers

---
+++ App/IsTableBusy.EntityFramework/DbMigrationHelpers.cs
@@ -0,0 +1,22 @@
+﻿namespace IsTableBusy.EntityFramework
+{
+    public static class DbMigrationHelpers
+    {
+        public static string DropDefaultConstraintSql(string table, string column)
+        {
+            return string.Format(@"
+        DECLARE @name sysname
+
+        SELECT @name = dc.name
+        FROM sys.columns c
+        JOIN sys.default_constraints dc ON dc.object_id = c.default_object_id
+        WHERE c.object_id = OBJECT_ID('{0}')
+        AND c.name = '{1}'
+
+        IF @name IS NOT NULL
+            EXECUTE ('ALTER TABLE {0} DROP CONSTRAINT ' + @name)
+        ",
+                table, column);
+        }
+    }
+}
