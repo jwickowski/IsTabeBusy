98879dcd324691cba8d4eec34024640bd6dd705b
Jerzy Wickowski <tazos333@gmail.com>
2017-12-19 14:08:42 +0100

    do some changes in main file, try to use states

--- App/IsTableBusy.Device.ESP2866/src/Main.cpp
+++ App/IsTableBusy.Device.ESP2866/src/Main.cpp
@@ -2,36 +2,36 @@
 #include <QueueArray.h>
 #include "Device/Button.h"
 #include "Device/Light.h"
+#include "Device/WifiConnector.h"
 
 #include "Api/ApiClient.h"
 #include "Api/UrlPreparer.h"
-#include "State.h"
-#include "Command.h"
-#include "Commands/ConnectToWifiCommand.h"
+
+#include "States/States.h"
 
 #define GREEN_LED 16 //D0
-#define RED_LED 14 //D5
+#define RED_LED 14   //D5
 #define BUTTON_TOP 0 //D3
 
-
-//QueueArray <Command> queue;
-Configuration* configuration;
-bool isBusy = true;
-
-Button* button;
-Light* green;
-Light* red;
+Button *button;
+Configuration *configuration;
+Light *green;
+Light *red;
 ApiClient *apiClient;
 UrlPreparer *urlPreparer;
-State currentState = State::initializing;
-ConnectToWifiCommand *connectToWifiCommand;
-void applyLed()
+WifiConnector *wifiConnector;
+States currentState = States::Setuping;
+bool isBusy = true;
+
+void applyLed(bool isBusy)
 {
-  if(isBusy){
+  if (isBusy)
+  {
     green->Off();
     red->On();
   }
-  else{
+  else
+  {
     green->On();
     red->Off();
   }
@@ -43,26 +43,80 @@ void setup()
   button = new Button(BUTTON_TOP);
   green = new Light(GREEN_LED);
   red = new Light(RED_LED);
- 
+
   configuration = new Configuration();
   urlPreparer = new UrlPreparer();
-connectToWifiCommand = new ConnectToWifiCommand(configuration -> GetWifiSsid(), configuration -> GetWifiPassword());
-  char* url = urlPreparer -> PrepareUrl();
+  wifiConnector->AddConnectionData(configuration->GetWifiSsid(), configuration->GetWifiPassword());
+  char *url = urlPreparer->PrepareUrl();
   apiClient = new ApiClient(url);
+  currentState = States::Initializing;
 }
 
-void loop(){
+void Initialize()
+{
+  bool ran = wifiConnector->Run();
+  if (ran)
+  {
+    currentState == States::Registering;
+  }
+}
+
+void Register()
+{
+  isBusy = apiClient->GetBusy();
+  applyLed(isBusy);
+  currentState == States::WaitingForClick;
+}
+
+void CheckClick()
+{
   bool isClicked = button->IsClicked();
-  if(isClicked){
-    Serial.println("click");
-    int ran = connectToWifiCommand->Execute();
-    if(ran == 0){
-      isBusy = apiClient -> GetBusy();
-      apiClient -> SetBusy(!isBusy);
-      applyLed();
+  if (isClicked)
+  {
+    if (isBusy)
+    {
+      currentState == States::UpdatingStatusToFree;
     }
-    else{
-      Serial.println("not-connected");
+    else
+    {
+      currentState == States::UpdatingStatusToBusy;
     }
+  }
+}
+
+void SetIsBusy(bool newIsBusyValue)
+{
+  isBusy = newIsBusyValue;
+  apiClient->SetBusy(isBusy);
+  applyLed(isBusy);
+}
+
+void loop()
+{
+  switch (currentState)
+  case States::Initializing:
+  {
+    Initialize();
+    break;
+  }
+  case States::Registering:
+  {
+    Register();
+    break;
+  }
+  case States::WaitingForClick:
+  {
+    CheckClick();
+    break;
+  }
+  case States::UpdatingStatusToBusy:
+  {
+    SetIsBusy(true);
+    break;
+  }
+  case States::UpdatingStatusToFree:
+  {
+    SetIsBusy(false);
+    break;
   }
 }
